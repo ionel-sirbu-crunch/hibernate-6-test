fef47e3e45e7bf24d2fb22bfaa01d99177886bdc
Ionel Sirbu <Ionel.Sirbu@crunch.co.uk>
2024-09-18 15:13:27 +0100

    Initial project setup.

---
+++ src/main/java/uk/co/crunch/hibernate/test/Hibernate6TestApplication.java
@@ -0,0 +1,13 @@
+package uk.co.crunch.hibernate.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hibernate6TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hibernate6TestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/crunch/hibernate/test/Hibernate6TestApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.crunch.hibernate.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hibernate6TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
